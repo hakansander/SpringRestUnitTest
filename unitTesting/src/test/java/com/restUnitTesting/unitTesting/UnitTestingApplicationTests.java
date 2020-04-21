1ca2e77771ce769b3272ab48240376418df352ad
Hakan Sander <hakansander@gmail.com>
2020-04-21 14:27:10 +0300

    Initial commit

---
+++ unitTesting/src/test/java/com/restUnitTesting/unitTesting/UnitTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restUnitTesting.unitTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
